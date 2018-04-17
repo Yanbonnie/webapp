1e0b56cfcec337c308c272a41d5cfa00ce3be7aa
Bonnie <user_reg@qq.com>
2018-04-17 18:56:59 +0800

    样式修改

---
+++ nanTang/pages/business/business.js
@@ -0,0 +1,76 @@
+// pages/business/business.js
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+        shop: { title: '商家', type: 'input', placeholder: '请输入商家名称', componentId:"shop" },
+        shopVal:'',
+        charge: { title: '负责人', type: 'input', placeholder: '请输入负责人姓名', componentId:"charge" },
+        chargeVal:'',
+        phone: { title: '联系电话', type: 'input', placeholder: '请输入联系电话', componentId:'phone' },
+        phoneVal:'',
+        phones: { title: '外卖电话', type: 'input', placeholder: '请输入外卖号码用,分隔多个', componentId: 'phones' },
+        phonesVal:'',
+        address: { title: '地址', type: 'input', placeholder: '请输入地址', componentId:'address' },
+        addressVal:'',
+        label: { title: '标签', type: 'input', placeholder: '请输入标签便于用户搜索', componentId:'label' },
+        labelVal:''
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function (options) {
+
+    },
+
+    submitInput(){
+        console.log(this.data.shopVal)
+    },
+    _handleZanFieldChange(e){
+        const { componentId } = e.currentTarget.dataset;
+        const { value } = e.detail;
+        switch(componentId){
+            case 'shop':
+                this.setData({
+                    shopVal: value
+                })
+                break;
+            case 'charge':
+                this.setData({
+                    chargeVal:value
+                })
+                break;
+            case 'phone':
+                this.setData({
+                    phoneVal: value
+                })
+                break;
+            case 'phones':
+                this.setData({
+                    phonesVal: value
+                })
+                break;
+            case 'address':
+                this.setData({
+                    addressVal: value
+                })
+                break;
+            case 'label':
+                this.setData({
+                    labelVal: value
+                })
+                break;
+            default:
+                break;
+        }
+    },
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function () {
+
+    }
+})
