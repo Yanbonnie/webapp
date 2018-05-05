a3e889a17a003dcfd5ee07a0bd4351c496b21bd8
Bonnie <user_reg@qq.com>
2018-05-05 23:12:10 +0800

    修改

---
+++ nanTang/utils/sweet.js
@@ -0,0 +1,39 @@
+/**
+ * 
+ * 
+ * pages/business/business                         我是商家
+ * 
+ * pages/manage/uploadCert/uploadCert              上传证件
+ * 
+ * pages/manage/index/index                        管理页面
+ * 
+ * pages/manage/classify/index/index               分类管理
+ * 
+ * pages/manage/classify/opera/opera               增加/编辑分类
+ * 
+ * pages/order/orderManage/orderManage             订单管理
+ * 
+ * pages/manage/goods/index/index                  商品管理
+ * 
+ * pages/manage/goods/opera/opera                  增加或编辑商品
+ * 
+ * pages/manage/member/index/index                 员工管理
+ * 
+ * pages/manage/member/scan/scan                   员工扫码页面
+ * 
+ * pages/order/placeOrder/placeOrder               用户点单页面
+ * 
+ * pages/order/submitOrder/submitOrder             选择地址提交页面
+ * 
+ * pages/manage/member/orderTaking/orderTaking     员工接单
+ * 
+ * pages/user/user                                 我的信息（用户个人中心）
+ * 
+ * pages/order/myOrder/myOrder                     我的订单
+ * 
+ * pages/order/orderDetail/orderDetail             订单详情
+ * 
+ * pages/order/orderManage/orderManage             订单管理
+ * 
+ * pages/manage/finance/finance                     财务管理
+ */
