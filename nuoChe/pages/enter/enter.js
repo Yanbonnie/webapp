c9d096af50d0efab289aedda07e4f16ba59bb88f
Bonnie <user_reg@qq.com>
2018-07-17 00:14:26 +0800

    修改

--- nuoChe/pages/enter/enter.js
+++ nuoChe/pages/enter/enter.js
@@ -13,41 +13,47 @@ Page({
     onLoad: function () {
         let openid = wx.getStorageSync('openid');
         if (openid){
-            wx.reLaunch({
-                url: `/pages/index/index?openid=${openid}`
-            })
-        }
-        if (app.globalData.userInfo) {
-            console.log(app.globalData)
             this.setData({
-                userInfo: app.globalData.userInfo.userInfo,
-                hasUserInfo: true
+                hasUserInfo:true
             })
-            this.loginHandle(app.globalData.userInfo);
-        } else if (this.data.canIUse) {
-            // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-            // 所以此处加入 callback 以防止这种情况
-            app.userInfoReadyCallback = res => {
+            setTimeout(()=>{
+                wx.reLaunch({
+                    url: `/pages/index/index?openid=${openid}`
+                })
+            },300)
+        }else{
+            if (app.globalData.userInfo) {
                 this.setData({
-                    userInfo: res.userInfo,
+                    userInfo: app.globalData.userInfo.userInfo,
                     hasUserInfo: true
                 })
-                this.loginHandle(res);
-            }
-        } else {
-            // 在没有 open-type=getUserInfo 版本的兼容处理
-            wx.getUserInfo({
-                success: res => {
-                    console.log(res)
-                    app.globalData.userInfo = res
+                this.loginHandle(app.globalData.userInfo);
+            } else if (this.data.canIUse) {
+                // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+                // 所以此处加入 callback 以防止这种情况
+                app.userInfoReadyCallback = res => {
                     this.setData({
                         userInfo: res.userInfo,
                         hasUserInfo: true
                     })
                     this.loginHandle(res);
                 }
-            })
+            } else {
+                // 在没有 open-type=getUserInfo 版本的兼容处理
+                wx.getUserInfo({
+                    success: res => {
+                        console.log(res)
+                        app.globalData.userInfo = res
+                        this.setData({
+                            userInfo: res.userInfo,
+                            hasUserInfo: true
+                        })
+                        this.loginHandle(res);
+                    }
+                })
+            }
         }
+        
     },
     getUserInfo: function (e) {
         console.log(e)
@@ -84,9 +90,11 @@ Page({
             let { openid } = res.data;
             wx.setStorageSync('openid', openid);  //设置缓存
             wx.hideLoading();
-            wx.redirectTo({
-                url: `/pages/index/index?openid=${openid}`
-            })
+            setTimeout(() => {
+                wx.reLaunch({
+                    url: `/pages/index/index?openid=${openid}`
+                })
+            }, 300)
         }).catch(res=>{
             
             if(count < 5){
